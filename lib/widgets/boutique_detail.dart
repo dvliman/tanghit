4da0506d77309ab1004c2c75af99725e2a30caac
David Liman <limanoit@gmail.com>
2020-01-04 18:10:23 -0800

    add app bar icon: favorite_border

--- lib/widgets/boutique_detail.dart
+++ lib/widgets/boutique_detail.dart
@@ -10,23 +10,46 @@ class BoutiqueDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text(vendor.name)),
+        appBar: AppBar(title: Text(vendor.name), actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.favorite_border),
+            onPressed: () {},
+          )
+        ]),
         body: Column(children: [
-          singleImage(vendor.mainPhoto)
+          singleImage(vendor.mainPhoto),
+          name(vendor.name),
+          description(vendor.description),
         ]));
   }
 
   Widget singleImage(String path) {
     return Center(
       child: Container(
-        height: 300,
-        width: 220,
-        alignment: Alignment.center,
-        child: FadeInImage(
-          placeholder: AssetImage("assets/images/placeholder.png"),
-          image: AssetImage(path),
-        )
-      ),
+          height: 300,
+          width: 220,
+          alignment: Alignment.center,
+          child: FadeInImage(
+            placeholder: AssetImage("assets/images/placeholder.png"),
+            image: AssetImage(path),
+          )),
     );
+  }
+
+  Widget name(String name) {
+    return Container(
+      padding: const EdgeInsets.only(left: 10.0),
+      alignment: Alignment.centerLeft,
+      child: Text(name,
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+    );
+  }
+
+  Widget description(String description) {
+    return Container(
+        padding: EdgeInsets.only(left: 10.0, top: 20.0),
+        alignment: Alignment.centerLeft,
+        child: Text(description,
+            style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal)));
   }
 }
