4a74cf4d3a0ffeef5fdac092d050f4d9205aac26
David Liman <limanoit@gmail.com>
2020-01-04 16:44:18 -0800

    boutique list

--- lib/widgets/boutique.dart
+++ lib/widgets/boutique.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:tanghit/data/vendor.dart';
 
 class Boutique extends StatelessWidget {
@@ -9,6 +9,62 @@ class Boutique extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(vendor.name);
+    return Card(
+      elevation: 3,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(8),
+        child: Container(
+          child: Row(
+            children: <Widget>[
+              boutiqueImage(vendor),
+              Expanded(child: boutiqueDescription(vendor, 100)),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+
+  Widget boutiqueImage(Vendor vendor) => ClipRRect(
+        child: FadeInImage(
+          placeholder: AssetImage(vendor.mainPhoto),
+          image: AssetImage(vendor.mainPhoto),
+          width: 100,
+          height: 100,
+          fit: BoxFit.cover,
+        ),
+      );
+
+  Widget boutiqueDescription(Vendor vendor, double height) => Container(
+        height: height,
+        padding: EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 8,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text(
+              vendor.name,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black87,
+                fontWeight: FontWeight.bold,
+              ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              vendor.description,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.black54,
+              ),
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+      );
 }
