3b011db07db21e823deb699048a1c7199beaff44
David Liman <limanoit@gmail.com>
2020-01-06 19:12:31 -0800

    boutique details

--- lib/widgets/boutique_detail.dart
+++ lib/widgets/boutique_detail.dart
@@ -12,44 +12,64 @@ class BoutiqueDetail extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(title: Text(vendor.name), actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.favorite_border),
+            icon: Icon(Icons.send),
             onPressed: () {},
           )
         ]),
         body: Column(children: [
-          singleImage(vendor.mainPhoto),
-          name(vendor.name),
-          description(vendor.description),
+          Expanded(flex: 3, child: buildImage(context, vendor.mainPhoto)),
+          buildOverview(vendor),
+          buildProperties(vendor),
+          buildInquiry(),
         ]));
   }
 
-  Widget singleImage(String path) {
-    return Center(
-      child: Container(
-          height: 300,
-          width: 220,
-          alignment: Alignment.center,
-          child: FadeInImage(
-            placeholder: AssetImage("assets/images/placeholder.png"),
-            image: AssetImage(path),
-          )),
-    );
-  }
-
-  Widget name(String name) {
-    return Container(
-      padding: const EdgeInsets.only(left: 10.0),
-      alignment: Alignment.centerLeft,
-      child: Text(name,
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-    );
-  }
-
-  Widget description(String description) {
-    return Container(
-        padding: EdgeInsets.only(left: 10.0, top: 20.0),
-        alignment: Alignment.centerLeft,
-        child: Text(description,
-            style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal)));
-  }
+  Widget buildImage(BuildContext context, String photo) => Container(
+        child: Image.asset(photo,
+            fit: BoxFit.fill, width: MediaQuery.of(context).size.width - 80),
+      );
 }
+
+ListTile buildOverview(Vendor vendor) => ListTile(
+    title: Text("Overview",
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+    subtitle: Text(vendor.description));
+
+Widget buildProperties(Vendor vendor) => Container(
+      decoration: BoxDecoration(border: Border.all(color: Color(0xffb0b0b0))),
+      padding: const EdgeInsets.all(8),
+      child: Table(
+        children: [
+          buildProperty("Category", maybeEmpty(vendor.category.toString())),
+          buildProperty("Email", maybeEmpty(vendor.email)),
+          buildProperty("Phone", maybeEmpty(vendor.phone)),
+          buildProperty("Homepage", maybeEmpty(vendor.homepage)),
+          buildProperty("Address", vendor.address),
+        ],
+      ),
+    );
+
+TableRow buildProperty(String prop, String value) => TableRow(children: [
+      TableCell(child: Text(prop)),
+      TableCell(child: Text(value, textAlign: TextAlign.end)),
+    ]);
+
+Widget buildInquiry() => Row(
+      children: [
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.black,
+            ),
+            child: FlatButton(
+                onPressed: () {},
+                child: Text(
+                  "Send inquiry",
+                  style: TextStyle(color: Colors.white),
+                )),
+          ),
+        ),
+      ],
+    );
+
+String maybeEmpty(String x) => x != null ? x : "";
