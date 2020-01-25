7adb9141596ffd81d866e1f50d3c45635983c2c9
David Liman <limanoit@gmail.com>
2020-01-25 14:44:49 -0800

    launch tel: or mailto: uri and handle error if can't launch (#8)

--- lib/screens/info.dart
+++ lib/screens/info.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class InfoScreen extends StatelessWidget {
   @override
@@ -15,12 +16,18 @@ class InfoScreen extends StatelessWidget {
                 title: Text("Support Center")
             ),
             InkWell(
+              onTap: () {
+                _call(context, '714-253-2851');
+              },
               child: ListTile(
                 leading: Icon(Icons.call),
                 title: Text("Call: (714) 253 2851")
               )
             ),
             InkWell(
+              onTap: () {
+                _email(context, 'support@tanghit.com');
+              },
               child: ListTile(
                 leading: Icon(Icons.mail),
                 title: Text("Email: support@tanghit.com")
@@ -29,5 +36,29 @@ class InfoScreen extends StatelessWidget {
           ]
         )
     );
+  }
+
+  _call(BuildContext context, String phone) async {
+    final uri = 'tel:$phone';
+
+    if (await canLaunch(uri)) {
+      await launch(uri);
+    } else {
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text("Failed to call: $phone")
+      ));
+    }
+  }
+
+  _email(BuildContext context, String email) async {
+    final uri = 'mailto:$email';
+
+    if (await canLaunch(uri)) {
+      await launch(uri);
+    } else {
+      Scaffold.of(context).showSnackBar(SnackBar(
+          content: Text("Failed to email: $email")
+      ));
+    }
   }
 }
