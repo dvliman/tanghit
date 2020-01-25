82d4eba783e940bf24aaeb12b03777dd25d8eb6f
David Liman <limanoit@gmail.com>
2020-01-25 14:52:24 -0800

    move info's body to info-sheet, and call from both: info-screen and send-inquiry-button

--- lib/screens/info.dart
+++ lib/screens/info.dart
@@ -1,7 +1,5 @@
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:tanghit/widgets/info_sheet.dart';
 
 class InfoScreen extends StatelessWidget {
   @override
@@ -10,55 +8,6 @@ class InfoScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text("Info"),
         ),
-        body: ListView(
-          children: [
-            ListTile(
-                title: Text("Support Center")
-            ),
-            InkWell(
-              onTap: () {
-                _call(context, '714-253-2851');
-              },
-              child: ListTile(
-                leading: Icon(Icons.call),
-                title: Text("Call: (714) 253 2851")
-              )
-            ),
-            InkWell(
-              onTap: () {
-                _email(context, 'support@tanghit.com');
-              },
-              child: ListTile(
-                leading: Icon(Icons.mail),
-                title: Text("Email: support@tanghit.com")
-              )
-            ),
-          ]
-        )
-    );
-  }
-
-  _call(BuildContext context, String phone) async {
-    final uri = 'tel:$phone';
-
-    if (await canLaunch(uri)) {
-      await launch(uri);
-    } else {
-      Scaffold.of(context).showSnackBar(SnackBar(
-        content: Text("Failed to call: $phone")
-      ));
-    }
-  }
-
-  _email(BuildContext context, String email) async {
-    final uri = 'mailto:$email';
-
-    if (await canLaunch(uri)) {
-      await launch(uri);
-    } else {
-      Scaffold.of(context).showSnackBar(SnackBar(
-          content: Text("Failed to email: $email")
-      ));
-    }
+        body: InfoSheet());
   }
 }
