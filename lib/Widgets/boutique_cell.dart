a0fd5aabb659cdbaa6097608a47ea182d54b4e09
David Liman <limanoit@gmail.com>
2020-01-25 21:12:48 -0800

    change the boutique-cell to be more like instagram (column oriented) (#9)
    * change the boutique-cell to be more like instagram (column oriented)
    
    * Added more spacing (#11)
    
    Co-authored-by: leohemanth <leohemanth@gmail.com>

--- lib/Widgets/boutique_cell.dart
+++ lib/Widgets/boutique_cell.dart
@@ -11,72 +11,52 @@ class BoutiqueCell extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () => {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => BoutiqueDetail(this.vendor)))
-      },
-      child: Card(
-        elevation: 2.0,
-        margin: EdgeInsets.all(1.0),
-        child: InkWell(
-          borderRadius: BorderRadius.circular(8),
-          child: Container(
-            child: Row(
-              children: <Widget>[
-                Flexible(child: BoutiqueImage(vendor)),
-                BoutiqueDescription(vendor, 100),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+        onTap: () => {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => BoutiqueDetail(this.vendor)))
+            },
+        child: Column(
+          children: <Widget>[
+            Row(children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 10, 10, 10),
+                child: CircleAvatar(
+                  radius: 20.0,
+                  backgroundImage: AssetImage(vendor.mainPhoto),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(vendor.name,
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    Flexible(
+                      child: Container(
+                        padding: EdgeInsets.fromLTRB(0,0,20,0),
+                        child: Text(
+                          vendor.address,
+                          textScaleFactor: 0.8,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ]),
+            AspectRatio(
+                aspectRatio: 4.0 / 3.0,
+                child: FadeInImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(vendor.mainPhoto),
+                  placeholder: AssetImage(vendor.mainPhoto),
+                )),
+          ],
+        ));
   }
-
-  Widget BoutiqueImage(Vendor vendor) => ClipRRect(
-        child: FadeInImage(
-          placeholder: AssetImage(vendor.mainPhoto),
-          image: AssetImage(vendor.mainPhoto),
-          width: 100,
-          height: 100,
-          fit: BoxFit.cover,
-        ),
-      );
-
-  Widget BoutiqueDescription(Vendor vendor, double height) => Expanded(
-        child: Container(
-          height: height,
-          padding: EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 8,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Text(
-                vendor.name,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.black87,
-                  fontWeight: FontWeight.bold,
-                ),
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-              ),
-              Text(
-                vendor.description,
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.black54,
-                ),
-                maxLines: 3,
-                overflow: TextOverflow.ellipsis,
-              ),
-            ],
-          ),
-        ),
-      );
 }
