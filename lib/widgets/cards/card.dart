352bfb89401dc3e297cbe46464e4779824dbbd95
devanandp <deva051195@gmail.com>
2020-04-17 09:18:26 +0530

    Format data again

--- lib/widgets/cards/card.dart
+++ lib/widgets/cards/card.dart
@@ -36,133 +36,136 @@ class _DashboardCardState extends State<DashboardCard> {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: CardPostModel.hardcordedPost.cardStatus ? Container(
-        color: Colors.transparent,
-        child: Column(
-          children: <Widget>[
-            CardHeader(),
-            Container(
-              child: Stack(
+      child: CardPostModel.hardcordedPost.cardStatus
+          ? Container(
+              color: Colors.transparent,
+              child: Column(
                 children: <Widget>[
-                  CarouselSlider(
-                    viewportFraction: 1.0,
-                    aspectRatio: MediaQuery.of(context).size.aspectRatio,
-                    height: 224,
-                    onPageChanged: (index) {
-                      setState(() {
-                        _current = index;
-                      });
-                    },
-                    items: imagelist.map((i) {
-                      return Builder(
-                        builder: (BuildContext context) {
-                          return Container(
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: AssetImage(i),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          );
-                        },
-                      );
-                    }).toList(),
-                  ),
-                  Positioned(
-                      bottom: 10,
-                      left: 187.5,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: map<Widget>(imagelist, (index, url) {
-                          return Container(
-                            width: 6.0,
-                            height: 6.0,
-                            margin: EdgeInsets.symmetric(
-                                vertical: 10.0, horizontal: 2.0),
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: _current == index
-                                    ? Colors.black
-                                    : Colors.grey),
-                          );
-                        }),
-                      )),
-                ],
-              ),
-            ),
-            Container(
-              height: 160,
-              child: Stack(
-                children: <Widget>[
-                  Positioned(
-                    left: 10,
-                    top: 20,
-                    child: Container(
-                      child: Text(
-                        CardPostModel.hardcordedPost.description,
-                        style: TextStyle(color: Colors.black, fontSize: 16),
-                      ),
-                      width: 400,
-                    ),
-                  ),
-                  Positioned(
-                    left: 10,
-                    top: 80,
-                    child: Text(
-                      CardPostModel.hardcordedPost.tags,
-                      style: TextStyle(
-                          fontStyle: FontStyle.italic,
-                          fontSize: 14,
-                          color: Colors.indigo),
-                    ),
-                  ),
-                  Positioned(
-                      left: 10.0,
-                      bottom: 20,
-                      child: Container(
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: <Widget>[
-                            GestureDetector(
-                              child: isClicked
-                                  ? Icon(
-                                      Icons.favorite,
-                                      color: Colors.red,
-                                    )
-                                  : Icon(Icons.favorite_border),
-                              onTap: () {
-                                setState(() {
-                                  isClicked = !isClicked;
-                                });
+                  CardHeader(),
+                  Container(
+                    child: Stack(
+                      children: <Widget>[
+                        CarouselSlider(
+                          viewportFraction: 1.0,
+                          aspectRatio: MediaQuery.of(context).size.aspectRatio,
+                          height: 224,
+                          onPageChanged: (index) {
+                            setState(() {
+                              _current = index;
+                            });
+                          },
+                          items: imagelist.map((i) {
+                            return Builder(
+                              builder: (BuildContext context) {
+                                return Container(
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: AssetImage(i),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                );
                               },
-                            ),
-                            SizedBox(
-                              width: 20,
-                            ),
-                            Icon(Icons.chat),
-                            SizedBox(
-                              width: 20,
-                            ),
-                            Icon(Icons.arrow_forward),
-                            SizedBox(
-                              width: 250,
-                            ),
-                            Image.asset(
-                              'assets/logo/whatsapp-logo-symbol-vector.png',
-                              height: 30,
-                              width: 30,
-                            ),
-                          ],
+                            );
+                          }).toList(),
                         ),
-                      )),
+                        Positioned(
+                            bottom: 10,
+                            left: 187.5,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: map<Widget>(imagelist, (index, url) {
+                                return Container(
+                                  width: 6.0,
+                                  height: 6.0,
+                                  margin: EdgeInsets.symmetric(
+                                      vertical: 10.0, horizontal: 2.0),
+                                  decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: _current == index
+                                          ? Colors.black
+                                          : Colors.grey),
+                                );
+                              }),
+                            )),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 160,
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned(
+                          left: 10,
+                          top: 20,
+                          child: Container(
+                            child: Text(
+                              CardPostModel.hardcordedPost.description,
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 16),
+                            ),
+                            width: 400,
+                          ),
+                        ),
+                        Positioned(
+                          left: 10,
+                          top: 80,
+                          child: Text(
+                            CardPostModel.hardcordedPost.tags,
+                            style: TextStyle(
+                                fontStyle: FontStyle.italic,
+                                fontSize: 14,
+                                color: Colors.indigo),
+                          ),
+                        ),
+                        Positioned(
+                            left: 10.0,
+                            bottom: 20,
+                            child: Container(
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: <Widget>[
+                                  GestureDetector(
+                                    child: isClicked
+                                        ? Icon(
+                                            Icons.favorite,
+                                            color: Colors.red,
+                                          )
+                                        : Icon(Icons.favorite_border),
+                                    onTap: () {
+                                      setState(() {
+                                        isClicked = !isClicked;
+                                      });
+                                    },
+                                  ),
+                                  SizedBox(
+                                    width: 20,
+                                  ),
+                                  Icon(Icons.chat),
+                                  SizedBox(
+                                    width: 20,
+                                  ),
+                                  Icon(Icons.arrow_forward),
+                                  SizedBox(
+                                    width: 250,
+                                  ),
+                                  Image.asset(
+                                    'assets/logo/whatsapp-logo-symbol-vector.png',
+                                    height: 30,
+                                    width: 30,
+                                  ),
+                                ],
+                              ),
+                            )),
+                      ],
+                    ),
+                  )
                 ],
               ),
             )
-          ],
-        ),
-      ) : Container(),
-    ) ;
+          : Container(),
+    );
   }
 }
 
